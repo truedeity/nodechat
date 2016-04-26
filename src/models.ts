34d9041444efda07a0c394e03d5f1b88840a48dc
=dan <hulkmedia@gmail.com>
2016-04-26 01:07:30 -0700

    update

--- src/models.ts
+++ src/models.ts
@@ -7,25 +7,25 @@ interface Message {
 }
 
 export class UserMessage implements Message {
-    private data: {name: string, message: string };
-    
+    private data: { name: string, message: string };
+
     constructor(payload: string) {
         var data = JSON.parse(payload);
-        
-        if(!data.name || !data.message) {
+
+        if (!data.name || !data.message) {
             throw new Error("some error")
         }
-        
+
         this.data = data;
     }
-    
-    
-    public get name() : string {
+
+
+    public get name(): string {
         return this.data.name;
     }
-    
-    public get message() : string {
+
+    public get message(): string {
         return this.data.message;
     }
-    
+
 }
